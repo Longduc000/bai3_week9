e0ec3b0fb6b3d0d25f24df8ddeb6ce12a280cab5
Longduc000 <119059985+Longduc000@users.noreply.github.com>
2024-10-31 02:08:09 +0700

    Add files via upload

---
+++ bai3/WiFi_Disconnect_QoS0_Publishing/src/mqtt.h
@@ -0,0 +1,7 @@
+namespace MQTT
+{
+    const char *username = "hivemq.webclient.1730313334795";
+    const char *password = "hivemq.webclient.1730313334795";
+    const char *broker = "69f695fc1c884f55872b72f864e38612.s1.eu.hivemq.cloud";
+    uint16_t port = 8883;
+}
---
+++ bai3/WiFi_Disconnect_QoS0_Publishing/src/wifi.h
@@ -0,0 +1,9 @@
+namespace WiFiSecrets
+{
+    const char *ssid = "Phong 302";
+    const char *pass = "33332222";
+    const char *echo_topic = "esp32/echo_test";
+    unsigned int publish_count = 0;
+    uint16_t keepAlive = 15;
+    uint16_t socketTimeout = 5;
+}
